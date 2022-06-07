fee5e513b6be398b97a932dc838796b510d57e01
chakio <y-kawasaki@keio.jp>
2022-06-08 01:10:50 +0900

    :sparkles: FEAT: echo fps of process

--- openpose_ros/script/openpose_ros.py
+++ openpose_ros/script/openpose_ros.py
@@ -6,10 +6,17 @@ import math
 import cv2
 import os
 from sys import platform
+import time
+import copy
+import datetime
+import ros_numpy
+import numpy as np
+
 import rospy
 # from hsrb_interface     import geometry
 import tf2_ros
 import tf2_py as tf2
+import tf
 
 from std_msgs.msg import Header
 from std_msgs.msg import String
@@ -22,21 +29,13 @@ import message_filters
 from sensor_msgs.msg import Image, CameraInfo
 import image_geometry
 
-import copy
-import tf
-import datetime
-import ros_numpy
-import numpy as np
 
 sys.path.append("/openpose/build/python")
-framenum=0
 try:
     from openpose import pyopenpose as op
 except ImportError as e:
         print('Error: OpenPose library could not be found. Did you enable `BUILD_PYTHON` in CMake and have this Python script in the right folder?')
         raise e
-    
-# 点の座標を定義するフレームの名前
 
 # PointCloud2のフィールドの一覧
 FIELDS = [
@@ -59,11 +58,13 @@ class OpenposeRos:
         image_topic = rospy.get_param('~image_topic', 'color/image_raw')
         depth_topic = rospy.get_param('~depth_topic', 'depth/image_raw')
         info_topic = rospy.get_param('~camera_info_topic', 'color/camera_info')
+        self.proc_image_width = rospy.get_param('~process_image_width', 1120)
         print(image_topic, depth_topic, info_topic)
         self.bridge      = CvBridge()
         color_sub = message_filters.Subscriber(image_topic, Image)
         depth_sub = message_filters.Subscriber(depth_topic, Image)
         info_sub = message_filters.Subscriber(info_topic, CameraInfo)
+
 
         ts = message_filters.TimeSynchronizer([color_sub, depth_sub, info_sub], 10)
         ts.registerCallback(self.msgFilterCallback)
@@ -71,9 +72,11 @@ class OpenposeRos:
         self.output_pub = rospy.Publisher("/openpose_image",Image, queue_size=1)
         self.camera_model = image_geometry.PinholeCameraModel()
 
+        self.average_process_time = 0
+
         params = dict()
         params["model_folder"] ="/openpose/models/"
-        params["net_resolution"] ="-1x144"
+        params["net_resolution"] ="-1x112"
         # Starting OpenPose
         self.opWrapper = op.WrapperPython()
         self.opWrapper.configure(params)
@@ -81,30 +84,35 @@ class OpenposeRos:
         rospy.loginfo("OpenposeRos setup completed")
     
     def msgFilterCallback(self, color_msg, depth_msg, info_msg):
-        poseKeypoints = self.getPose(color_msg)
+
+        start = time.time()
+        cv_color = self.bridge.imgmsg_to_cv2(color_msg, "bgr8")
+        [cv_color, scale_rate] = self.scale_image(cv_color, self.proc_image_width)
+        poseKeypoints = self.getPose(cv_color)
         
         if poseKeypoints is not None:
             # print("get3DPose")
+            
             kernel = np.ones((10, 10), np.uint8)
             self.camera_model.fromCameraInfo(info_msg)
             depth_msg.encoding='mono16'
-            depth_img = self.bridge.imgmsg_to_cv2(depth_msg, "mono16")
+            cv_depth = self.bridge.imgmsg_to_cv2(depth_msg, "mono16")
+            # [cv_depth, scale_rate] = self.scale_image(cv_depth, self.proc_image_width)
             # depth_img = cv2.GaussianBlur(depth_img,(5,5),0)
             # depth_img = cv2.blur(depth_img,(50,50))
             # depth_img = cv2.medianBlur(depth_img, 5)
-            depth_img = cv2.dilate(depth_img,kernel)
+            # depth_img = cv2.dilate(depth_img,kernel)
             # print(depth_msg.header)
             # print(color_msg.header)
-            pose_pcd = self.get3DPose(poseKeypoints, depth_img, self.camera_model, depth_msg.header)
+            pose_pcd = self.get3DPose(poseKeypoints, cv_depth, self.camera_model, depth_msg.header, scale_rate)
             if pose_pcd is not None:
                 self.publish3DPose(pose_pcd)
+            end = time.time()
+
+            self.average_process_time = 0.9*self.average_process_time + 0.1*(end-start)
+            rospy.loginfo("FPS: "+str(1.0/self.average_process_time) + " Process Time: "+str(self.average_process_time)) 
                 
-    def getPose(self, colorMsg):
-        try:
-            cv_image = self.bridge.imgmsg_to_cv2(colorMsg, "bgr8")
-        except CvBridgeError as e:
-            print('Cv_Bridge_Error')
-            return None
+    def getPose(self, cv_image):
         datum = op.Datum()
         datum.cvInputData = cv_image
         self.opWrapper.emplaceAndPop(op.VectorDatum([datum]))
@@ -117,7 +125,14 @@ class OpenposeRos:
             print(e)
         return poseKeypoints
 
-    def get3DPose(self,keypoints, depth_img, camera_model, header):
+    def scale_image(self, input_image, output_width):
+        h, w = input_image.shape[:2]
+        rate = output_width / w
+        output_height = round(h * rate)
+        output_image = cv2.resize(input_image, dsize=(output_width, output_height))
+        return [output_image, rate]
+
+    def get3DPose(self,keypoints, depth_img, camera_model, header, scale_rate):
         genPoints=[]
         minimumConfidence=0.01
 
@@ -131,17 +146,17 @@ class OpenposeRos:
             for jointNum,jointData in enumerate(personData):
                 if minimumConfidence>jointData[2]:
                     continue
-                x_index = round(jointData[0])
-                y_index = round(jointData[1])
+                
+                x_index = round(jointData[0]/scale_rate)
+                y_index = round(jointData[1]/scale_rate)
                 # print(depth_img.shape)
-                if depth_img.shape[1] > x_index:
+                if depth_img.shape[1] <= x_index:
                     x_index = depth_img.shape[1] - 1
-                if depth_img.shape[0] > y_index:
+                if depth_img.shape[0] <= y_index:
                     y_index = depth_img.shape[0] - 1
-
+                depth = depth_img[y_index,x_index]
                 # print("人番号："+str(personNum)+", 関節番号："+str(jointNum)+", x："+str(jointData[0])+", y："+str(jointData[1])+", depth："+str(depth_img[y_index,x_index])+", confidence："+str(jointData[2])) 
 
-                depth = depth_img[y_index,x_index]
                 x = (x_index - center_x) * depth * constant_x
                 y = (y_index - center_y) * depth * constant_y
                 z = depth/1000.0
